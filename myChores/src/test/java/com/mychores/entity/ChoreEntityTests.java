e9aa59911e5641a1016ee996c3c0d30cf041eef7
Raul Chiari <rchiari@rchiari-mac.slc.ere>
2017-06-05 13:26:38 -0600

    initial commit

---
+++ myChores/src/test/java/com/mychores/entity/ChoreEntityTests.java
@@ -0,0 +1,19 @@
+package com.mychores.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.DEFINED_PORT)
+public class ChoreEntityTests {
+
+  @Test
+  public void testMe() {
+    assertThat(true).isTrue();
+  }
+}
---
+++ myChores/start.sh
@@ -0,0 +1,1 @@
+mvn spring-boot:run -Drun.jvmArguments="-XX:MaxPermSize=128m -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5007"
